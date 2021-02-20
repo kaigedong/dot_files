3df3cfd0ab1ec65481679e52b341272cb5cdb80a
bobo <dongkaige@gmail.com>
2021-02-20 12:03:28 +0800

    fix: use abbr in fish

--- .config/fish/conf.d/omf.fish
+++ .config/fish/conf.d/omf.fish
@@ -3,19 +3,16 @@ set -q XDG_DATA_HOME
   and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
   or set -gx OMF_PATH "$HOME/.local/share/omf"
 
-# alias
-alias zshconfig="mate ~/.zshrc"
-alias ohmyzsh="mate ~/.oh-my-zsh"
-alias ls="lsd"
-alias la="ls -lth --date \"+%Y-%m-%d %H:%M:%S\""
-alias lb="ls -lh"
-alias grep="rg"
-alias less="bat -p"
-alias baidunetdisk="/usr/lib/baidunetdisk/baidunetdisk"
-alias top="glances"
-
 # Load Oh My Fish configuration.
 source $OMF_PATH/init.fish
 source $OMF_PATH/pkg/fish/conf.d/dracula.fish
 
 # starship init fish | source
+
+# alias
+abbr -a la "lsd -lth --date \"+%Y-%m-%d %H:%M:%S\""
+abbr -a lb "la -a"
+abbr -a grep rg
+abbr -a less "bat -p"
+abbr -a baidunetdisk /usr/lib/baidunetdisk/baidunetdisk
+abbr -a top glances
