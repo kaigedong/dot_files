e92c50bac8b92f4521e5f3af31a00e0e8f785860
bobo <dongkaige@gmail.com>
2021-03-06 13:32:10 +0800

    feat: add i3 config

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -22,6 +22,9 @@
 ;; (setq doom-font (font-spec :family "NotoSansMono Nerd Font" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
 
+(setq doom-font (font-spec :family "NotoSansMono Nerd Font" :size 12 :weight 'semi-light)
+      doom-variable-pitch-font (font-spec :family "sans" :size 13))
+
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
