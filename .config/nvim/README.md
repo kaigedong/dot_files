e0d4fc633c7e870764867790d6ef4efe11d619ac
kaigedong <dongkaige@gmail.com>
2022-01-04 10:28:31 +0800

    更新文档

--- .config/nvim/README.md
+++ .config/nvim/README.md
@@ -8,10 +8,36 @@
 
 ## 插件
  1. LspInfo 显示当前buffer attach的LSP
- 2. LspInstallInfo 显示所有Lsp; `i`--install，`u`--update，`U`--update all; `X` -- uninstall 
+ 2. LspInstallInfo 显示所有Lsp; `i`--install，`u`--update，`U`--update all; `X` -- uninstall
  3. 开启/禁用treesitter: TSDisableAll heightlight / TSEnableAll heightlight
 
 ## 快捷键
  1. g + l 可以显示LSP提示信息
  2. <S-v> 可以选中当前行
 
+## 主题
+
+1. tmux中，要开启24-color支持: `tmux_conf_24b_colour=true`
+
+使用下面命令，输出连续不间断的色彩才能正确显示nvim色彩
+
+```bash
+awk 'BEGIN{
+   s="/\\/\\/\\/\\/\\"; s=s s s s s s s s;
+   for (colnum = 0; colnum<77; colnum++) {
+       r = 255-(colnum*255/76);
+       g = (colnum*510/76);
+       b = (colnum*255/76);
+       if (g>255) g = 510-g;
+       printf "\033[48;2;%d;%d;%dm", r,g,b;
+       printf "\033[38;2;%d;%d;%dm", 255-r,255-g,255-b;
+       printf "%s\033[0m", substr(s,colnum+1,1);
+   }
+   printf "\n";
+}'
+```
+
+2. 同时，打开tmux的时候，要指定环境变量：在.zshrc中配置：
+  `alias tmux="TERM=xterm-256color tmux"`
+
+3. vim.notify如果被其他消息覆盖，可以执行`:messages`命令，来查看
--- .config/nvim/lua/user/colorscheme.lua
+++ .config/nvim/lua/user/colorscheme.lua
@@ -1,30 +1,8 @@
 local colorscheme = "tokyonight"
 
+-- vim.cmd "colrscheme darkblue" 或 :colorscheme darkblue
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
     vim.notify("colorscheme " .. colorscheme .. " not found!")
     return
 end
-
--- 1. `:colorscheme TAB`可以选择一些内置的主题
--- 2. 上面的代码，相当于代码： vim.cmd "colrscheme darkblue"
--- 的增强版，等于在命令行执行： :colorscheme darkblue
--- 3. tmux中，要开启24-color支持:
--- tmux_conf_24b_colour=true
--- 使用下面命令，输出连续不间断的色彩才能正确显示nvim色彩
--- awk 'BEGIN{
---     s="/\\/\\/\\/\\/\\"; s=s s s s s s s s;
---     for (colnum = 0; colnum<77; colnum++) {
---         r = 255-(colnum*255/76);
---         g = (colnum*510/76);
---         b = (colnum*255/76);
---         if (g>255) g = 510-g;
---         printf "\033[48;2;%d;%d;%dm", r,g,b;
---         printf "\033[38;2;%d;%d;%dm", 255-r,255-g,255-b;
---         printf "%s\033[0m", substr(s,colnum+1,1);
---     }
---     printf "\n";
--- }'
--- 4. 同时，打开tmux的时候，要指定环境变量：在.zshrc中配置：
--- alias tmux="TERM=xterm-256color tmux"
--- 5. 上面的vim.notify如果被其他消息冲掉，可以执行`:messages`命令，来查看
